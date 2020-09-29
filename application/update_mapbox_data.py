cd1f9d6836705c801664334db497624402727752
tps48 <tps48@case.edu>
2020-09-29 16:57:55 -0400

    update map to show covid data

--- application/update_mapbox_data.py
+++ application/update_mapbox_data.py
@@ -1,7 +1,14 @@
 import requests
 import json
 import sys
+import os
 from app import db
 from models import Flight, Airport, County, State
 
-states = 
+#load the mapbox tileset
+script_dir = os.path.dirname(__file__)
+f = open(script_dir + '/json/stateData.geojson', 'r')
+contents = f.read()
+f.close()
+mapbox_data = json.loads(contents)
+print(len(mapbox_data['features']))
