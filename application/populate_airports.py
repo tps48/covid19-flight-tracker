d1d618cdc28647882969f6a3ab35387906431a40
tps48 <tps48@case.edu>
2020-10-01 15:11:03 -0400

    fix bug in populate_airports script

--- application/populate_airports.py
+++ application/populate_airports.py
@@ -9,17 +9,13 @@ import xlrd
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
+#from Sets import set
 
 print('Deleted '+str(Airport.query.delete())+' airports')
 airportID = 0
 
 territories = ["AMERICAN SAMOA", "N MARIANA ISLANDS", "GUAM", "MIDWAY ATOLL",
-    "PUERTO RICO", "VIRGIN ISLANDS"]
-
-def assignNewAirportID():
-    temp = airportID
-    airportID = aiportID + 1
-    return temp 
+    "PUERTO RICO", "VIRGIN ISLANDS", "AMERICAN"]
 
 # delete existing entries
 # def reset():
@@ -29,6 +25,7 @@ def assignNewAirportID():
 # read excel file as store all info as dataframe
 #if using p139 file, there's no need to implement a filter func
 airportFile = open('./application/data/p139certifiedAirports.xlsx','rb')
+state_set = set()
 
 fileData = pd.read_excel(airportFile)
 
@@ -36,38 +33,40 @@ for index, row in fileData.iterrows():
 
     stateName = ''
     #check for territories (DC gets modded to virginia)
-    if row['StateName'] in territories:
-        break
-    elif row['StateName'] == "DIST. OF COLUMBIA":
-        stateName = 'DISTRICT OF COLUMBIA'
-    else:
-        stateName = row['StateName']
-    
-    try:
+    if row['StateName'] not in territories:
 
-        airportState = db.session.query(State).filter(func.lower(State.name) == func.lower(stateName)).all()
-        if len(airportState) > 0:
-            airportState = airportState[0]
-            print('State: ' + airportState.name)
-            
-            countyName = row['County']
-            airportCounty = db.session.query(County).filter(County.state_id == airportState.id).filter(County.name == func.lower(countyName)).all()
-            if len(airportCounty) > 0:
-                airportCounty = airportCounty[0]
-                print('County: ' + airportCounty.name)
+        if row['StateName'] == "DIST. OF COLUMBIA":
+            stateName = 'DISTRICT OF COLUMBIA'
+        else:
+            stateName = row['StateName']
+            state_set.add(stateName)
+        
+        try:
 
-                airport = Airport(
-                    id = assignNewAirportID(),
-                    icao24 = row['IcaoIdentifier'],
-                    county_id = airportCounty.id,
-                    code = func.lower(row['FacilityName'])
-                )
-                print(row['FacilityName'])
-                db.session.add(airport)
+            airportState = db.session.query(State).filter(func.lower(State.name) == func.lower(stateName)).all()
+            if len(airportState) > 0:
+                airportState = airportState[0]
+                print('State: ' + airportState.name)
+                
+                countyName = row['County']
+                print(countyName)
+                airportCounty = db.session.query(County).filter(County.state_id == airportState.id).filter(func.lower(County.name) == func.lower(countyName)).all()
+                if len(airportCounty) > 0:
+                    airportCounty = airportCounty[0]
+                    print('County: ' + airportCounty.name)
 
-    except Exception as e:
-        print('ERROR: Unable to add airport ' + str(e))
+                    airport = Airport(
+                        icao24 = row['IcaoIdentifier'],
+                        county_id = airportCounty.id,
+                        code = func.lower(row['LocationID'][1:])
+                    )
+                    print(row['FacilityName'])
+                    db.session.add(airport)
+
+        except Exception as e:
+            print('ERROR: Unable to add airport ' + str(e))
 
 db.session.commit()
+print('{} states'.format(len(state_set)))
 
 
