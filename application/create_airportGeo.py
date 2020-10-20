9c7afc622ad79acf097a5892500a05fd8a9ad464
Jack Bilotti <jtb129@case.edu>
2020-10-19 21:20:48 -0400

    airport geoJSON

---
+++ application/create_airportGeo.py
@@ -0,0 +1,20 @@
+from app import db
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from models import Airport
+from geojson import Point, Feature, FeatureCollection, dump
+from sqlalchemy import func
+
+airportQuery = db.session.query(Airport).all()
+features = []
+
+for airport in airportQuery:
+    point = Point((float(airport.latitude), float(airport.longitude)))
+    
+    features.append(Feature(geometry=point, properties={"name": airport.code}))
+
+feature_collection = FeatureCollection(features)
+
+with open('./json/airportData.geojson', 'w') as f:
+    dump(feature_collection, f)
+    
